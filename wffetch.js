3b77be722b58ede2f7b508f986d9469e46c05c56
Xalicus Xander <Links.Retreat@Gmail.com>
2024-11-12 01:59:38 -0700

    Got it working now.

--- wffetch.js
+++ wffetch.js
@@ -8,7 +8,7 @@ window.onload = function() {
 	
 	.then(function(json) {
 		
-		var tableCode = '<table><caption>Warframes and their abilities</caption><thead><tr><th>.</th><th>Warframe</th><th>Ability 1</th><th>Ability 2</th><th>Ability 3</th><th>Ability 4</th></tr></thead><tbody>';
+		var tableCode = '<table><caption>Warframes and their abilities</caption><thead><tr><th></th><th>Warframe</th><th>Ability 1</th><th>Ability 2</th><th>Ability 3</th><th>Ability 4</th></tr></thead><tbody>';
 		
 		for (var i = 0; i < json.length; i++) {
 			tableCode += '<tr><td>' + json[i].photo + '</td><td>' + json[i].wfname + '</td><td>' + json[i].ability1 + '</td><td>' + json[i].ability2 + '</td><td>' + json[i].ability3 + '</td><td>' + json[i].ability4 + '</td></tr>';
