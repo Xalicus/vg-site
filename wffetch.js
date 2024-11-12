cf0d54363dde355b3a018b74695042d59300f687
Xalicus Xander <Links.Retreat@Gmail.com>
2024-11-12 01:22:37 -0700

    Testing original table js

--- wffetch.js
+++ wffetch.js
@@ -1,27 +1,4 @@
 window.onload = function() {
-	
-	fetch('warframes.json')
-	
-	.then(function(response) {
-		return response.json();
-	})
-	
-	.then(function(json) {
-		
-		var tableCode = '<table><caption>Warframes and their abilities</caption><thead><tr><th>.</th><th>Warframe:</th><th>Ability 1:</th><th>Ability 2:</th><th>Ability 3:</th><th>Ability 4:</th></tr></thead><tbody>';
-		
-		for (var i = 0; i < json.length; i++) {
-			tablecode += '<tr><td>' + json[i].photo + '</td><td>' + json[i].wfname + '</td><td>' + json[i].ability1 + '</td><td>' + json[i].ability2 + '</td><td>' + json[i].ability3 + '</td><td>' + json[i].ability4 + '</td></tr>';
-		}
-		
-		tableCode += '</tbody><tfoot><tr><td colspan="6">Source: https://www.warframe.com/game/warframes</td></tr></tfoot></table>';
-		
-		document.getElementById('tableWF').innerHTML = tableCode;
-	});
-};
-
-
-window.onload = function() {
 
     fetch('bibliography.json')
  
