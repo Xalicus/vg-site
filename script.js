3a9dce4ea7ca720cf2d7a25e259d40d6b218fb53
Xalicus Xander <Links.Retreat@Gmail.com>
2024-11-12 05:56:25 -0700

    Working site.
    All working for HTML, CSS, JavaScript, and JSON.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
- function gaming() {
-	 alert("Thanks for showing your support for gaming!")
- }
- 
- 
- let btnOpen = document.querySelector('.btnURL')
- let input = document.querySelector('.btnURLText')
- 
- btnOpen.addEventListener('click', () => {
-	 window.open(input.value, '_blank');
- });
+function gaming() {
+    alert("Thanks for showing your support for gaming!")
+}
+
+
+let btnOpen = document.querySelector('.btnURL')
+let input = document.querySelector('.btnURLText')
+
+btnOpen.addEventListener('click', () => {
+    window.open(input.value, '_blank');
+});
